5ef9a4f52453f1e7864fd037de1d3c7418ff3793
Zaven Mnatzakanian <zmnatz@gmail.com>
2019-04-29 09:38:17 -0400

    Added slug 7s date

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import {Row, Col} from 'antd';
-import Product from './Product'
+import React from "react";
+import { Row, Col } from "antd";
+import Product from "./Product";
 
 export default () => (
   <React.Fragment>
-    <h3>Upcoming Events</h3>
+    <h3>Events</h3>
     <Row type="flex" gutter={20}>
       <Col>
         <Product title="Gorge Cup" link="/store/gorgeCup">
-          <div>East Columbia Library</div>
+          <div>Troy Park</div>
           <div>April 27, 2019</div>
           <div>$150 Per Team</div>
         </Product>
       </Col>
       <Col>
         <Product title="Slug 7s" link="/store/slug7s">
-          <div>Date: TBD</div>
+          <div>Columbia, MD</div>
+          <div>June 15, 2019</div>
+          <div>$225 per team</div>
         </Product>
       </Col>
     </Row>
-  </React.Fragment >
-)
+  </React.Fragment>
+);
