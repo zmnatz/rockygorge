84d05748ad699e7be106253fd4fbdd9085a240b4
Zaven Mnatzakanian <zmnatz@gmail.com>
2019-05-02 07:04:59 -0400

    Updated config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from "react";
 import { Layout } from "antd";
-import { Router, Link } from '@reach/router'
-import Home from './routes/Home';
-import Store from './routes/Store'
-import Dues from './components/Products/Dues'
-import 'antd/dist/antd.css';
+import { Router, Link } from "@reach/router";
+import Home from "./routes/Home";
+import Store from "./routes/Store";
+import Dues from "./components/Products/Dues";
+import "antd/dist/antd.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Layout.Header>
-          <Link to="/"><h1>Rocky Gorge Rugby</h1></Link>
+          <Link to="/">
+            <h1>Rocky Gorge Rugby</h1>
+          </Link>
         </Layout.Header>
         <Layout.Content>
           <Router className="ant-row-flex ant-row-flex-center">
-            <Home default/>
-            <Store path='/store/*' />
-            <Dues path='/dues' />
+            <Home default />
+            <Store path="/store/*" />
+            <Dues path="/dues" />
           </Router>
         </Layout.Content>
-      </div >
+      </div>
     );
   }
 }
