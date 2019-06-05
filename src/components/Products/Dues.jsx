61d5f2e951860b66f296f9db8bf1fb0218feea01
Zaven Mnatzakanian <zmnatz@gmail.com>
2019-06-05 09:33:19 -0400

    Added fall dues

--- src/components/Products/Dues.jsx
+++ src/components/Products/Dues.jsx
@@ -1,44 +1,82 @@
-import React from 'react';
+import React from "react";
 
 export default () => (
   <div>
-    <h2>Spring Dues</h2>
+    <h2>Fall Dues</h2>
     <p>Pay your dues!</p>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-      <input type="hidden" name="cmd" value="_s-xclick"/>
-      <input type="hidden" name="hosted_button_id" value="LQ8YXA8BWNSF6"/>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <input type="hidden" name="on0" value="Dues Options"/>
-              Dues Options
-            </td>
-          </tr>
-          <tr><td><select name="os0">
-            <option value="Spring Dues">Spring Dues $200.00 USD</option>
-            <option value="Spring Dues and Donation">Spring Dues and Donation $250.00 USD</option>
-          </select> </td></tr>
-        </tbody>
-      </table>
 
-      <input type="hidden" name="currency_code" value="USD"/>
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+    <form
+      action="https://www.paypal.com/cgi-bin/webscr"
+      method="post"
+      target="_top"
+    >
+      <input type="hidden" name="cmd" value="_s-xclick" />
+      <input type="hidden" name="hosted_button_id" value="LQ8YXA8BWNSF6" />
+      <table>
+        <tr>
+          <td>
+            <input type="hidden" name="on0" value="Dues Options" />
+            Dues Options
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <select name="os0">
+              <option value="Fall Dues">Fall Dues $200.00 USD</option>
+              <option value="Fall Dues and Donation">
+                Fall Dues and Donation $250.00 USD
+              </option>
+            </select>
+          </td>
+        </tr>
+      </table>
+      <input type="hidden" name="currency_code" value="USD" />
+      <input
+        type="image"
+        src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif"
+        border="0"
+        name="submit"
+        alt="PayPal - The safer, easier way to pay online!"
+      />
+      <img
+        alt=""
+        border="0"
+        src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+        width="1"
+        height="1"
+      />
     </form>
-    <br/><br/>
+    <br />
+    <br />
     <h2>GOD Donations</h2>
     <p>
-      Your club needs you. Help out Rocky Gorge Rugby and donate today. 
-      Your donation lets Rocky Gorge buy new equipment, rent fields and 
-      help the club grow. Donate today to lord your contributions over lesser 
-      GODS and gloat to the young whippersnappers about how you saved their butts.
+      Your club needs you. Help out Rocky Gorge Rugby and donate today. Your
+      donation lets Rocky Gorge buy new equipment, rent fields and help the club
+      grow. Donate today to lord your contributions over lesser GODS and gloat
+      to the young whippersnappers about how you saved their butts.
     </p>
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+    <form
+      action="https://www.paypal.com/cgi-bin/webscr"
+      method="post"
+      target="_top"
+    >
       <input type="hidden" name="cmd" value="_s-xclick" />
       <input type="hidden" name="hosted_button_id" value="UAHLMT3W9ABYQ" />
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-      <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+      <input
+        type="image"
+        src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+        border="0"
+        name="submit"
+        title="PayPal - The safer, easier way to pay online!"
+        alt="Donate with PayPal button"
+      />
+      <img
+        alt=""
+        border="0"
+        src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+        width="1"
+        height="1"
+      />
     </form>
   </div>
-)
+);
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import {Col} from 'antd';
-import Listings from '../components/Listings'
-import Product from '../components/Product'
+import React from "react";
+import { Col } from "antd";
+import Listings from "../components/Listings";
+import Product from "../components/Product";
 
 export default () => (
   <React.Fragment>
-    <Col md={22} lg={11} style={{ textAlign: 'center' }}>
+    <Col md={22} lg={11} style={{ textAlign: "center" }}>
       <h2>Event Registration</h2>
-      <img
-        src="images/banquet.png"
-        alt="Banquet Banner"
-        width="300px"
-      />
+      <img src="images/banquet.png" alt="Banquet Banner" width="300px" />
       <h2>2012 and 2014 Division II National Champions</h2>
       <p className="description">
-        This is our storefront for signing up for upcoming events.
-        Register for upcoming tournaments, social events, and fundraisers.
+        This is our storefront for signing up for upcoming events. Register for
+        upcoming tournaments, social events, and fundraisers.
       </p>
     </Col>
     <Col md={22} lg={11}>
       <Listings />
       <h3>Team Dues</h3>
-      <Product title="Spring Dues" link="/dues">
-        Spring 2018 Mens Club Dues
+      <Product title="Fall Dues" link="/dues">
+        Fall 2019 Mens Club Dues
       </Product>
     </Col>
   </React.Fragment>
-)
+);
