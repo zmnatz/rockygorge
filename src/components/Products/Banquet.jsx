ac5ee2c7deedcc6448d0ec464aa035d97885baf6
Zaven Mnatzakanian <zmnatz@gmail.com>
2022-04-13 08:57:59 -0400

    added some banquet info

--- src/components/Products/Banquet.jsx
+++ src/components/Products/Banquet.jsx
@@ -18,10 +18,21 @@ export default function Banquet() {
       options={OPTIONS}
       description="Banquet Tickets"
     >
-      <Typography.Title level={3}>
+      <Typography.Title level={2}>
         Join us in celebrating the 10th anniversary of our club's first National
         Championship.
       </Typography.Title>
+      <Typography.Title level={4}>When: June 4, 6pm - 10pm</Typography.Title>
+      <Typography.Title level={4}>
+        Where: The Meeting House, 5885 Robert Oliver Place, Columbia MD
+      </Typography.Title>
+
+      <Typography>
+        Enjoy food, drinks and entertainment with master of ceremony, our very
+        own comedian Erik Woodworth. The agenda includes awards for the 2022
+        season, honoring our Rocky Gorge Hall of Fame and a tribute video to the
+        championship produced by Evan Lappen.
+      </Typography>
     </PaypalProduct>
   );
 }
