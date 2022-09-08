44f4f1d328805caaf7fc5fdb8a59c35cea394bfa
Zaven Mnatzakanian <zmnatz@gmail.com>
2022-09-08 10:10:43 -0400

    Update Savannah.jsx

--- src/components/Products/Savannah.jsx
+++ src/components/Products/Savannah.jsx
@@ -1,10 +1,10 @@
 import PaypalProduct from "../PaypalProduct";
 
 export default function Savannah() {
-  return <PaypalProduct defaultAmount={200} description="Savannah Tournament">
+  return <PaypalProduct defaultAmount={275} description="Savannah Tournament">
     <p>
-      Rocky Gorge is headed to Savannah to play rugby. $200 covers
-      transporation, tournament fee and hotels. If making your own
+      Rocky Gorge is headed to Savannah to play rugby. $275 covers
+      the bus, tournament fee and hotels. If making your own
       arrangements, contact Joel Henry and pay your part of the tournaent
       fee ($20).
     </p>
