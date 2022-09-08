71ca772ed76aa0c8c09faaa4e91255d66aaf422a
Zaven Mnatzakanian <zmnatz@gmail.com>
2022-09-08 10:13:08 -0400

    Update PlayerInfo.jsx

--- src/components/Sections/PlayerInfo.jsx
+++ src/components/Sections/PlayerInfo.jsx
@@ -6,27 +6,15 @@ export default (_) => (
   <>
     <Row type="flex">
       <Col>
-        <Product title="Spring Banquet" link="/banquet">
-          <Typography>June 4: $65 Tickets</Typography>
-          <Typography>
-            Celebrating the 10th anniversary of our National Championship.
-          </Typography>
-        </Product>
-      </Col>
-      <Col>
-        <Product title="Slugfest 10s" link="/slugfest">
-          <Typography>June 25: $300 per team</Typography>
-          <Typography>
-            Men's, Women's and U19 divisions.
-          </Typography>
-        </Product>
-      </Col>
-      <Col>
         <Product title="Player Dues" link="/dues">
           <Typography>Fall Season: $200</Typography>
           <Typography>$25 off for new players</Typography>
         </Product>
       </Col>
+      <Col>
+        <Product title="Savannah Trip" link="/savannah">
+          <Typography>St Patricks Day Tournament</Typography>
+          <Typography>$275: Bus, hotels, tournament fees</Typography>
       <Col>
         <Product title="Advertise with Us" link="/newSponsors">
           <Typography>Become a jersey sponsor.</Typography>
