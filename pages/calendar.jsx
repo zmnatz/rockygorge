d54ff517b00d2cacd064dbbe77f7b0cdcfa0d011
Zaven <zmnatz@gmail.com>
2024-02-12 11:21:13 -0500

    Update calendar.jsx

--- pages/calendar.jsx
+++ pages/calendar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-
 export default function Calendar () {
   return (
+    <>
+            <p>Click Subscribe in the bottom right of the calendar for android. For iPhone, <a href="https://calendar.google.com/calendar/ical/t1s0l2ucm1fl084olhmj69pme8%40group.calendar.google.com/public/basic.ics">Click Here to Subscribe</a>.</p>
     <iframe
       title="Rocky Gorge Team Calendar"
       src="https://calendar.google.com/calendar/embed?src=t1s0l2ucm1fl084olhmj69pme8%40group.calendar.google.com&ctz=America%2FNew_York&showTitle=0"
@@ -13,5 +13,7 @@ export default function Calendar () {
         scrolling: "no"
       }}
     />
+
+      </>
   );
 };
