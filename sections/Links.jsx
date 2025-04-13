e57a4a69aba9357c0271de30921f7ad9e6f359bb
Zaven <zmnatz@gmail.com>
2025-04-13 06:26:08 -0400

    Update Links.jsx

--- sections/Links.jsx
+++ sections/Links.jsx
@@ -1,6 +1,7 @@
 import { Row} from "antd";
 import CardLink from "../components/CardLink";
 import links from "../data/links.yml";
+import forms from "../data/forms.yml"
 
 export default function Links () {
   return (
@@ -10,6 +11,17 @@ export default function Links () {
           key={item?.link}
           title={item?.title}
           link={item?.link}
+        >
+          {item.description}
+        </CardLink>
+      ))}
+    </Row>
+    <Row type="flex">
+      {forms.filter(item => item.hide !== true).map((item) => (
+        <CardLink 
+          key={item?.id}
+          title={item?.name}
+          link={'forms/${item?.id}'}
         >
           {item.description}
         </CardLink>
